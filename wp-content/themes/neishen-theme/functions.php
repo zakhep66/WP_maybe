7b5f66bcd6e0ceca356a6b56eb42a835883fcdeb
zakhep66 <66241982+zakhep66@users.noreply.github.com>
2020-12-25 18:51:21 +0300

    version num 2

--- wp-content/themes/neishen-theme/functions.php
+++ wp-content/themes/neishen-theme/functions.php
@@ -1,3 +1,22 @@
 <?php 
 
-wp_enqueue_style( 'main', get_template_directory_uri() . '/css/main.css' );
+wp_enqueue_style( 'animate', get_template_directory_uri() . '/css/animate.css' );
+wp_enqueue_style( 'bootstrap-grid.min', get_template_directory_uri() . '/css/bootstrap-grid.min.css' );
+wp_enqueue_style( 'style', get_template_directory_uri() . '/css/style.css' );
+wp_enqueue_style( 'slick', get_template_directory_uri() . '/slick/slick.css' );
+wp_enqueue_style( 'slick-theme', get_template_directory_uri() . '/slick/slick-theme.css' );
+
+
+
+add_action('wp_enqueue_scripts', 'my_scripts_method'); // когда ВП начинает подставлять скрипты, мы ципляемся за wp_enqueue_scripts и подставляем my_scripts_method
+function my_scripts_method(){
+    //отменаем зареганную jquery
+    //вместо 'jquery-core' можно вписать 'jquery', тогда будет отменён ещё и jquery-migrate (для версий jquery 1.9 и ниже)
+    wp_deregister_script( 'jquery' );
+    wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js' );
+    wp_enqueue_script( 'jquery' );
+}
+
+wp_enqueue_script('slick',    get_template_directory_uri() . '/slock/slick.min.js', 'jquery',    null,           true);
+//                название     это имточник(папка с темой)      расположение     зависит от     указывать      подключить в футере?
+//                скрипта                                                                   или нет версию
---
+++ wp-content/themes/neishen-theme/mailer/smart.php
@@ -0,0 +1,45 @@
+<?php 
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$text = $_POST['user_text'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'tot.samyj.994@mail.ru';                 // Наш логин
+$mail->Password = 'assassin666';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('tot.samyj.994@mail.ru', 'Тот Самый');   // От кого письмо 
+$mail->addAddress('maxklimov13@yandex.ru');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Новая заявка';
+$mail->Body    = '
+	Пользователь оставил свои данные <br> 
+	Имя: ' . $name . ' <br> 
+	Телефон: ' . $phone . ' <br> 
+	Какие требуются материалы: ' . $text . '';
+$mail->AltBody = 'Это альтернативный текст';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    echo 'Yes';
+}
+
+?>
